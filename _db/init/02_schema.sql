ed2bb811594103faa08edc91f4f8bc0c56ac5ece
Will Charczuk <will@clotheshor.se>
2016-03-16 12:42:57 -0700

    reordering tables.

--- _db/init/02_schema.sql
+++ _db/init/02_schema.sql
@@ -14,6 +14,26 @@ ALTER TABLE users ADD CONSTRAINT pk_users_id PRIMARY KEY (id);
 ALTER TABLE users ADD CONSTRAINT uk_users_uuid UNIQUE (uuid);
 ALTER TABLE users ADD CONSTRAINT uk_users_username UNIQUE (username);
 
+CREATE TABLE user_auth (
+    user_id bigint not null,
+    provider varchar(32) not null,
+    timestamp_utc timestamp not null,
+    auth_token bytea not null,
+	auth_token_hash bytea not null,
+    auth_secret bytea
+);
+ALTER TABLE user_auth ADD CONSTRAINT pk_user_auth_user_id_provider PRIMARY KEY (user_id,provider);
+ALTER TABLE user_auth ADD CONSTRAINT fk_user_auth_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+CREATE INDEX ix_user_auth_auth_token_hash ON user_auth(auth_token_hash);
+
+CREATE TABLE user_session (
+	session_id varchar(32) not null,
+    user_id bigint not null,
+    timestamp_utc timestamp not null
+);
+ALTER TABLE user_session ADD CONSTRAINT pk_user_session_session_id PRIMARY KEY (session_id);
+ALTER TABLE user_session ADD CONSTRAINT fk_user_session_user_id FOREIGN KEY (user_id) REFERENCES users(id);
+
 CREATE TABLE image (
 	id serial not null,
 	uuid varchar(32) not null,
@@ -79,23 +99,3 @@ ALTER TABLE vote_log ADD CONSTRAINT fk_vote_log_image_id
 	FOREIGN KEY (image_id) REFERENCES image(id);
 ALTER TABLE vote_log ADD CONSTRAINT fk_vote_log_tag_id
 	FOREIGN KEY (tag_id) REFERENCES tag(id);
-    
-CREATE TABLE user_auth (
-    user_id bigint not null,
-    provider varchar(32) not null,
-    timestamp_utc timestamp not null,
-    auth_token bytea not null,
-	auth_token_hash bytea not null,
-    auth_secret bytea
-);
-ALTER TABLE user_auth ADD CONSTRAINT pk_user_auth_user_id_provider PRIMARY KEY (user_id,provider);
-ALTER TABLE user_auth ADD CONSTRAINT fk_user_auth_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-CREATE INDEX ix_user_auth_auth_token_hash ON user_auth(auth_token_hash);
-
-CREATE TABLE user_session (
-	session_id varchar(32) not null,
-    user_id bigint not null,
-    timestamp_utc timestamp not null
-);
-ALTER TABLE user_session ADD CONSTRAINT pk_user_session_session_id PRIMARY KEY (session_id);
-ALTER TABLE user_session ADD CONSTRAINT fk_user_session_user_id FOREIGN KEY (user_id) REFERENCES users(id);
