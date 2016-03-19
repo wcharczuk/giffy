2bdc455a397c61a13b55c54337eb0f6077cc336d
Will Charczuk <will@clotheshor.se>
2016-03-18 19:28:51 -0700

    things.

--- _db/add_migrate_utility.sh
+++ _db/add_migrate_utility.sh
@@ -6,5 +6,5 @@
 # FOR EACH FILE IN ./migrate_utility (these are necessary for migrations)
 for file in $(find ./_db/migrate_utility -name "*.sql" -type f);
 do
-    ssh dokku@${GIFFY_HOST} postgres:connect ${GIFFY_APP} < ${FILE}
+    ssh dokku@${GIFFY_HOST} postgres:connect ${GIFFY_APP} < ${file}
 done
