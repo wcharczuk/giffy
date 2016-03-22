2a65ba08abd5f6a338fd6cab80ce0f8afeac6b3f
Will Charczuk <will@clotheshor.se>
2016-03-21 20:37:06 -0700

    things

--- server/core/external_request.go
+++ server/core/external_request.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"fmt"
+	"net/url"
 
 	"github.com/blendlabs/go-request"
 )
@@ -9,6 +10,8 @@ import (
 // NewExternalRequest creates a new external request.
 func NewExternalRequest() *request.HTTPRequest {
 	return request.NewHTTPRequest().OnResponse(func(meta *request.HTTPResponseMeta, body []byte) {
-		fmt.Printf("External Response -- %s\n", string(body))
+		fmt.Printf("External Request Response -- %s\n", string(body))
+	}).OnRequest(func(verb string, url *url.URL) {
+		fmt.Printf("External Request: %s %s", verb, url.String())
 	})
 }
