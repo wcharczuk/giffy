bc388d9680d7e711771f9ed5ca2859e0b61ddb9a
Will Charczuk <will@clotheshor.se>
2016-03-28 12:04:35 -0700

    missed a few.

--- _util/coverage.sh
+++ _util/coverage.sh
@@ -7,11 +7,11 @@ echo "mode: set" > ./profile.cov
 for dir in $(find ${ROOT} -maxdepth 10 -not -path '*/testdata' -type d);
 do
 if ls $dir/*.go &> /dev/null; then
-    genv -f="./_config/config.json" go test -short -covermode=set -coverprofile=$dir/profile.tmp $dir
-    if [ -f $dir/profile.tmp ]; then
-        cat $dir/profile.tmp | tail -n +2 >> profile.cov
-        rm $dir/profile.tmp
-    fi
+	genv -f="./_config/config.json" go test -short -covermode=set -coverprofile=$dir/profile.tmp $dir
+	if [ -f $dir/profile.tmp ]; then
+		cat $dir/profile.tmp | tail -n +2 >> profile.cov
+		rm $dir/profile.tmp
+	fi
 fi
 done
 
