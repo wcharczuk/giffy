a96a25e47e8afb04704d676fcff803d4395acbf8
Will Charczuk <will@clotheshor.se>
2016-03-28 11:52:05 -0700

    every minute job.

--- server/jobs/delete_orphaned_tags.go
+++ server/jobs/delete_orphaned_tags.go
@@ -1,11 +1,13 @@
 package jobs
 
 import (
+	"time"
+
 	"github.com/blendlabs/go-chronometer"
 	"github.com/wcharczuk/giffy/server/model"
 )
 
-// OrphanedTags is a job that deletes orphaned tags
+// DeleteOrphanedTags is a job that deletes orphaned tags
 type DeleteOrphanedTags struct{}
 
 // Name returns the job name
@@ -15,7 +17,7 @@ func (ot DeleteOrphanedTags) Name() string {
 
 // Schedule returns the job schedule.
 func (ot DeleteOrphanedTags) Schedule() chronometer.Schedule {
-	return chronometer.EveryHour()
+	return chronometer.Every(1 * time.Minute)
 }
 
 // Execute runs the job
