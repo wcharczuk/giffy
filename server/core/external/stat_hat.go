c9bc31aaf0174ea0a8a9f27e9b80aea23e3b20e2
Will Charczuk <will@clotheshor.se>
2016-03-31 12:23:55 -0700

    no more stathat _local

--- server/core/external/stat_hat.go
+++ server/core/external/stat_hat.go
@@ -10,27 +10,33 @@ import (
 
 // StatHatRequestTiming posts the request timing to stat hat.
 func StatHatRequestTiming(timing time.Duration) {
-	statHatToken := core.ConfigStathatToken()
-	if len(statHatToken) != 0 {
-		requestTimingBucket := fmt.Sprintf("request_timing_%s", core.ConfigEnvironment())
-		stathat.PostEZValue(requestTimingBucket, statHatToken, float64(timing/time.Millisecond))
+	if core.ConfigEnvironment() != "local" {
+		statHatToken := core.ConfigStathatToken()
+		if len(statHatToken) != 0 {
+			requestTimingBucket := fmt.Sprintf("request_timing_%s", core.ConfigEnvironment())
+			stathat.PostEZValue(requestTimingBucket, statHatToken, float64(timing/time.Millisecond))
+		}
 	}
 }
 
 // StatHatError posts the request timing to stat hat.
 func StatHatError() {
-	statHatToken := core.ConfigStathatToken()
-	if len(statHatToken) != 0 {
-		errorCountBucket := fmt.Sprintf("error_count_%s", core.ConfigEnvironment())
-		stathat.PostEZCount(errorCountBucket, statHatToken, 1)
+	if core.ConfigEnvironment() != "local" {
+		statHatToken := core.ConfigStathatToken()
+		if len(statHatToken) != 0 {
+			errorCountBucket := fmt.Sprintf("error_count_%s", core.ConfigEnvironment())
+			stathat.PostEZCount(errorCountBucket, statHatToken, 1)
+		}
 	}
 }
 
 //StatHatSearch logs a search.
 func StatHatSearch() {
-	statHatToken := core.ConfigStathatToken()
-	if len(statHatToken) != 0 {
-		seachCountBucket := fmt.Sprintf("search_count_%s", core.ConfigEnvironment())
-		stathat.PostEZCount(seachCountBucket, statHatToken, 1)
+	if core.ConfigEnvironment() != "local" {
+		statHatToken := core.ConfigStathatToken()
+		if len(statHatToken) != 0 {
+			seachCountBucket := fmt.Sprintf("search_count_%s", core.ConfigEnvironment())
+			stathat.PostEZCount(seachCountBucket, statHatToken, 1)
+		}
 	}
 }
