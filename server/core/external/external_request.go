e0a8f7e57d9dac1bcc68c83dd2f1207ac7d48eea
Will Charczuk <will@clotheshor.se>
2016-04-06 17:22:11 -0700

    removing debugging stuff.

--- server/core/external/external_request.go
+++ server/core/external/external_request.go
@@ -1,14 +1,8 @@
 package external
 
-import (
-	"fmt"
-
-	"github.com/blendlabs/go-request"
-)
+import "github.com/blendlabs/go-request"
 
 // NewRequest creates a new external request.
 func NewRequest() *request.HTTPRequest {
-	return request.NewHTTPRequest().OnResponse(func(meta *request.HTTPResponseMeta, content []byte) {
-		fmt.Printf("Response: %s\n", string(content))
-	})
+	return request.NewHTTPRequest()
 }
