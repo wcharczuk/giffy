ed88e467b9afa24e4609a57f9e91bf21e82a849d
Will Charczuk <will@clotheshor.se>
2016-04-14 18:05:28 -0700

    test failures.

--- server/model/user_test.go
+++ server/model/user_test.go
@@ -20,3 +20,17 @@ func TestGetUsersByCountAndOffset(t *testing.T) {
 	assert.Nil(err)
 	assert.NotEmpty(users)
 }
+
+func TestGetAllUsers(t *testing.T) {
+	assert := assert.New(t)
+	tx, err := spiffy.DefaultDb().Begin()
+	assert.Nil(err)
+	defer tx.Rollback()
+
+	_, err = CreateTestUser(tx)
+	assert.Nil(err)
+
+	all, err := GetAllUsers(tx)
+	assert.Nil(err)
+	assert.NotEmpty(all)
+}
