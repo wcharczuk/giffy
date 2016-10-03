ac557d3ad0ea685ca9642ed09ad2cb0ef8f138ef
Will Charczuk <will@blendlabs.com>
2016-10-02 21:39:13 -0400

    more thorough test.

--- server/controller/upload_image_test.go
+++ server/controller/upload_image_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 	"testing"
 
 	assert "github.com/blendlabs/go-assert"
@@ -49,4 +50,20 @@ func TestUploadImageByPostedFile(t *testing.T) {
 	}).WithHeader(auth.SessionParamName, session.SessionID).FetchResponse()
 	assert.Nil(err)
 	assert.Equal(http.StatusOK, res.StatusCode)
+	assert.NotNil(res.Body)
+
+	defer res.Body.Close()
+
+	resContents, err := ioutil.ReadAll(res.Body)
+	assert.Nil(err)
+	assert.True(strings.Contains(string(resContents), "SUCCESS"))
+
+	imagesByUser, err := model.GetImagesForUserID(session.UserID, tx)
+	assert.Nil(err)
+	assert.NotEmpty(imagesByUser)
+	assert.Equal("image.gif", imagesByUser[0].DisplayName)
+	assert.Equal(58285, imagesByUser[0].FileSize)
+	assert.Equal(275, imagesByUser[0].Width)
+	assert.Equal(364, imagesByUser[0].Height)
+	assert.NotEmpty(imagesByUser[0].MD5)
 }
