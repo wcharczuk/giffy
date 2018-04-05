9070f8042fad14b5ffec072c566eb7247df63b72
Will Charczuk <will@blendlabs.com>
2018-04-04 23:18:53 -0700

    wiping deps for a minute

--- vendor/github.com/blendlabs/go-google-oauth/oauth_response.go
+++
@@ -1,9 +0,0 @@
-package google
-
-// OAuthResponse is a response from google oauth.
-type OAuthResponse struct {
-	AccessToken string `json:"access_token"`
-	TokenType   string `json:"token_type"`
-	ExpiresIn   int    `json:"expires_in"`
-	IDToken     string `json:"id_token"`
-}
