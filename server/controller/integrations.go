5d070894d8d3471bc1e38c5daa3d939f31c643d6
Will Charczuk <wcharczuk@users.noreply.github.com>
2021-07-31 13:02:46 -0700

    updates

--- server/controller/integrations.go
+++ server/controller/integrations.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"context"
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
@@ -15,13 +16,14 @@ import (
 )
 
 const (
-	slackContenttypeJSON      = "application/json; charset=utf-8"
-	slackContentTypeTextPlain = "text/plain; charset=utf-8"
-	slackErrorInvalidQuery    = "Please type at least (3) characters."
-	slackErrorBadPayload      = "There was an error processing a payload from slack. Saddness."
-	slackErrorInvalidAction   = "An invalid action was passed to the button handler."
-	slackErrorInternal        = "There was an error processing your request. Sadness."
-	slackErrorTeamDisabled    = "Your team has been disabled; contact the integration owner to re-enable."
+	slackContenttypeJSON           = "application/json; charset=utf-8"
+	slackContentTypeTextPlain      = "text/plain; charset=utf-8"
+	slackErrorInvalidQuery         = "Please type at least (3) characters."
+	slackErrorBadPayload           = "There was an error processing a payload from slack. Saddness."
+	slackErrorInvalidAction        = "An invalid action was passed to the button handler."
+	slackErrorInvalidCallbackState = "An invalid callback state was passed to the button handler."
+	slackErrorInternal             = "There was an error processing your request. Sadness."
+	slackErrorTeamDisabled         = "Your team has been disabled; contact the integration owner to re-enable."
 
 	slackActionShuffle = "shuffle"
 	slackActionPost    = "post"
@@ -56,11 +58,16 @@ func (i Integrations) slack(rc *web.Ctx) web.Result {
 
 	res := slackMessage{}
 	if strings.HasPrefix(args.Query, "img:") {
+		res.DeleteOriginal = true
+		res.AsUser = true
+		res.ResponseType = "in_channel"
+		res.AuthorName = args.UserName
 		res.ResponseType = "in_channel"
 		res.Attachments = []interface{}{
 			slackImageAttachment{Title: args.Query, ImageURL: result.S3ReadURL},
 		}
 	} else {
+		res.ReplaceOriginal = true
 		res.ResponseType = "ephemeral"
 		var title string
 		if len(result.Tags) > 0 {
@@ -112,27 +119,31 @@ func (i Integrations) slackErrorNoResults() string {
 }
 
 func (i Integrations) slackShuffle(payload slackActionPayload, rc *web.Ctx) web.Result {
-	query, uuid := i.extractCallbackState(payload.CallbackID)
-	contentRatingFilter, errRes := i.contentRatingFilter(payload.Team.ID, rc)
-	if errRes != nil {
-		return errRes
+	query, uuid := i.parseCallbackID(payload.CallbackID)
+	if query == "" || uuid == "" {
+		return web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInvalidCallbackState))
+	}
+	contentRatingFilter, err := i.getContentRatingForTeamID(rc.Context(), payload.Team.ID)
+	if err != nil {
+		logger.MaybeFatal(i.Log, err)
+		return web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInternal))
 	}
 
 	logger.MaybeInfof(i.Log, "search query: %s, excludes: %s", query, uuid)
 	result, err := i.Model.SearchImagesBestResult(rc.Context(), query, []string{uuid}, contentRatingFilter)
 	if err != nil {
+		logger.MaybeFatal(i.Log, err)
 		return web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInternal))
 	}
-
 	if result == nil || result.IsZero() {
 		return web.RawWithContentType(slackContentTypeTextPlain, []byte(i.slackErrorNoResults()))
 	}
-
 	output := viewmodel.NewImage(*result, i.Config)
 
-	res := slackMessage{}
-	res.ReplaceOriginal = true
-	res.ResponseType = "ephemeral"
+	res := slackMessage{
+		ReplaceOriginal: true,
+		ResponseType:    "ephemeral",
+	}
 	var title string
 	if len(result.Tags) > 0 {
 		title = output.Tags[0].TagValue
@@ -144,7 +155,6 @@ func (i Integrations) slackShuffle(payload slackActionPayload, rc *web.Ctx) web.
 		slackImageAttachment{Title: title, ImageURL: output.S3ReadURL},
 		i.buttonActions(query, result.UUID),
 	}
-
 	return i.renderResult(res, rc)
 }
 
@@ -159,25 +169,28 @@ func (i Integrations) slackPost(payload slackActionPayload, rc *web.Ctx) web.Res
 		)
 	}()
 
-	_, uuid := i.extractCallbackState(payload.CallbackID)
+	_, uuid := i.parseCallbackID(payload.CallbackID)
+	if uuid == "" {
+		return web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInvalidCallbackState))
+	}
 
 	img, err := i.Model.GetImageByUUID(rc.Context(), uuid)
 	if err != nil {
+		logger.MaybeFatal(i.Log, err)
 		return web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInternal))
 	}
-
 	if img == nil || img.IsZero() {
 		return web.RawWithContentType(slackContentTypeTextPlain, []byte(i.slackErrorNoResults()))
 	}
 
 	result := viewmodel.NewImage(*img, i.Config)
 
-	res := slackMessage{}
-	res.DeleteOriginal = true
-	res.AsUser = true
-	res.ResponseType = "in_channel"
-	res.AuthorName = payload.User.Name
-
+	res := slackMessage{
+		DeleteOriginal: true,
+		AsUser:         true,
+		ResponseType:   "in_channel",
+		AuthorName:     payload.User.Name,
+	}
 	var title string
 	if len(result.Tags) > 0 {
 		title = result.Tags[0].TagValue
@@ -187,7 +200,6 @@ func (i Integrations) slackPost(payload slackActionPayload, rc *web.Ctx) web.Res
 	res.Attachments = []interface{}{
 		slackImageAttachment{Title: title, ImageURL: result.S3ReadURL},
 	}
-
 	return i.renderResult(res, rc)
 }
 
@@ -210,18 +222,15 @@ func (i Integrations) slackEvent(rc *web.Ctx) web.Result {
 // Slack Helpers
 // --------------------------------------------------------------------------------
 
-func (i Integrations) contentRatingFilter(teamID string, rc *web.Ctx) (int, web.Result) {
-	contentRatingFilter := model.ContentRatingNR
-	teamSettings, err := i.Model.GetSlackTeamByTeamID(rc.Context(), teamID)
+func (i Integrations) getContentRatingForTeamID(ctx context.Context, teamID string) (contentRatingFilter int, err error) {
+	contentRatingFilter = model.ContentRatingNR
+	var slackTeam *model.SlackTeam
+	slackTeam, err = i.Model.GetSlackTeamByTeamID(ctx, teamID)
 	if err != nil {
-		logger.MaybeFatal(i.Log, err)
-		return contentRatingFilter, web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInternal))
-	} else if !teamSettings.IsZero() {
-		if !teamSettings.IsEnabled {
-			return contentRatingFilter, web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorTeamDisabled))
-		}
-
-		contentRatingFilter = teamSettings.ContentRatingFilter
+		return
+	}
+	if slackTeam != nil && !slackTeam.IsZero() {
+		contentRatingFilter = slackTeam.ContentRatingFilter
 	}
 	return contentRatingFilter, nil
 }
@@ -249,9 +258,10 @@ func (i Integrations) getResult(args slackArguments, rc *web.Ctx) (*viewmodel.Im
 		logger.MaybeTrigger(rc.Context(), i.Log, model.NewSearchHistoryDetailed("slack", args.TeamID, args.TeamName, args.ChannelID, args.ChannelName, args.UserID, args.UserName, args.Query, foundResult, resultID, tagID))
 	}()
 
-	contentRatingFilter, errRes := i.contentRatingFilter(args.TeamID, rc)
-	if errRes != nil {
-		return nil, errRes
+	contentRatingFilter, err := i.getContentRatingForTeamID(rc.Context(), args.TeamID)
+	if err != nil {
+		logger.MaybeFatal(i.Log, err)
+		return nil, web.RawWithContentType(slackContentTypeTextPlain, []byte(slackErrorInternal))
 	}
 
 	if strings.HasPrefix(args.Query, "img:") {
@@ -290,7 +300,7 @@ func (i Integrations) buttonActions(query, imageUUID string) slackActionAttachme
 	return slackActionAttachment{
 		Text:           "Hit either `Post` or `Shuffle` (for a new image).",
 		Fallback:       "Unable to do image things.",
-		CallbackID:     i.callbackState(query, imageUUID),
+		CallbackID:     i.createCallbackID(query, imageUUID),
 		AttachmentType: "default",
 		Actions: []slackAction{
 			{
@@ -316,15 +326,15 @@ func (i Integrations) buttonActions(query, imageUUID string) slackActionAttachme
 	}
 }
 
-func (i Integrations) callbackState(query, uuid string) string {
+func (i Integrations) createCallbackID(query, uuid string) string {
 	return fmt.Sprintf("%s||%s", base64.StdEncoding.EncodeToString([]byte(query)), uuid)
 }
 
-func (i Integrations) extractCallbackState(callbackID string) (query, uuid string) {
-	if len(callbackID) == 0 {
+func (i Integrations) parseCallbackID(callbackState string) (query, uuid string) {
+	if len(callbackState) == 0 {
 		return
 	}
-	parts := strings.SplitN(callbackID, "||", 2)
+	parts := strings.SplitN(callbackState, "||", 2)
 	if len(parts) < 2 {
 		return
 	}
